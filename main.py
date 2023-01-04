a89c6ee963e5ce50a054a0365ec644c69ea171fa
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2023-01-04 14:13:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import openai
+
+# Set the API key
+openai.api_key = "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"
+
+# Set the model to use
+model_engine = "text-davinci-002"
+
+# Set the prompt
+prompt = "What is the weather like today?"
+
+# Set the number of completions
+num_completions = 1
+
+# Set the temperature (between 0 and 1)
+temperature = 0.5
+
+# Set the top_p value (between 0 and 1)
+top_p = 1
+
+# Set the frequency_penalty value (between 0 and 1)
+frequency_penalty = 0
+
+# Set the presence_penalty value (between 0 and 1)
+presence_penalty = 0
+
+# Send the request
+completions = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=num_completions,
+    temperature=temperature,
+    top_p=top_p,
+    frequency_penalty=frequency_penalty,
+    presence_penalty=presence_penalty
+)
+
+# Print the response
+print(completions.choices[0].text)
